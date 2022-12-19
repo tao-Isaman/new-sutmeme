1aa48e71ed34be7a2f269169e19889a74456f133
tao-Isaman <towlovelove@gmail.com>
2022-12-19 21:04:47 +0700

    update tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <ul>
+      <li>
+        <NuxtLink to="/home">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/form">Form</NuxtLink>
+      </li>
+    </ul>
+    <NuxtPage />
+  </div>
 </template>
